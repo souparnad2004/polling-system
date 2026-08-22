48793a9c72bc02a7189f9994ef8b0d4a4bd2c1ef
Souparna Dhara <189111059+souparnad2004@users.noreply.github.com>
2026-08-22 21:58:18 +0530

    feat: add PostgreSQL database integration

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,12 +3,13 @@ import {z} from "zod";
 const envSchema = z.object({
     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
     PORT: z.coerce.number().min(1).max(65535).default(3000).describe("port must be a number between 1 and 65535"),  
+    DATABASE_URL: z.url().min(1).describe("database url must be a valid url"),
 })
 
 const result = envSchema.safeParse(process.env);
 
 if(!result.success) {
-    console.error("Invalid environment variables", z.treeifyError(result.error));
+    console.error("Invalid environment variables", z.flattenError(result.error));
     process.exit(1);
 }
 
