406bd27545701b27c03fe0e96a85c29889a7a593
Souparna Dhara <189111059+souparnad2004@users.noreply.github.com>
2026-08-23 12:21:46 +0530

    feat: initialize frontend with Next.js and React Query - Implement RootLayout component for application layout with metadata. - Create HomePage component with a title and description for the polling system. - Set up Providers component to manage React Query client. - Implement API client for fetching data from the backend.

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Providers from "./provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Polling System",
+  description: "Realtime Polling Platform",
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Polling System</h1>
+
+      <p>Create polls, vote and see results in rea life</p>
+    </main>
+  );
+}
