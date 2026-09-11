c3305e9fc314b0f0f8bca77bb7aa88f0da37e48b
Souparna Dhara <189111059+souparnad2004@users.noreply.github.com>
2026-09-11 21:44:45 +0530

    Update CORS origin to use environment variable

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ export function createApp(appDependencies = dependencies): express.Express {
     app.use(cookieParser());
     app.use(express.json());
     app.use(cors({
-        origin: "http://localhost:3000",
+        origin:process.env.FRONTEND_URL,
         credentials: true
     }));
 
